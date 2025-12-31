b9b80b37368c5b2b46611379cf74c8f7a7a39caa
milad8798 <bankaidex0@gmail.com>
2025-12-31 15:03:26 +0330

    Add files via upload

--- HEX.py
+++ HEX.py
@@ -1,29 +1,39 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام 👋 ربات روی Railway اجرا شد!")
+    keyboard = [
+        [
+            InlineKeyboardButton("🎮 مولتی", callback_data="multi"),
+            InlineKeyboardButton("⚔️ بتل", callback_data="battle")
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
+    await update.message.reply_text(
+        "یکی از گزینه‌ها رو انتخاب کن 👇",
+        reply_markup=reply_markup
+    )
+
+async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "multi":
+        await query.message.reply_text("🎮 وارد بخش مولتی شدی")
+    elif query.data == "battle":
+        await query.message.reply_text("⚔️ وارد بخش بتل شدی")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.add_handler(CallbackQueryHandler(buttons))
 
     app.run_polling()
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
